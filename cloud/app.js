15cd7292d4cda5bb5ab625cebd8cea5143344bce
handong890 <handong890@gmail.com>
2015-05-16 16:58:06 +0800

    v0.5

--- cloud/app.js
+++ cloud/app.js
@@ -6,6 +6,6 @@ app.set('views','cloud/views');
 app.set('view engine', 'ejs');
 app.use(express.bodyParser());
 
-require('./routes/indexRoute')(app);
+require('cloud/routes/indexRoute')(app);
 
 app.listen();
